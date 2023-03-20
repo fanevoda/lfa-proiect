809db2b681dbf985e18528f41373ee09d30f0dbb
fanevoda <59056171+fanevoda@users.noreply.github.com>
2023-03-20 20:37:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # citire
-
+import copy
 f = open("input.in", 'r')
 date_intrare = f.read().split('\n')
 f.close()
@@ -9,18 +9,32 @@ f.close()
 automat = {}
 starifinale = []
 cuvinte = []
+cititstare = False
 
 for linie in date_intrare:
     linie = linie.split()
     if len(linie) > 2:
-        automat[tuple([linie[0], linie[1]])] = linie[2]
+        if tuple([linie[0], linie[1]]) not in automat:
+            automat[tuple([linie[0], linie[1]])] = [linie[2]]
+        else:
+            automat[tuple([linie[0], linie[1]])].append(linie[2])
     try:
         if linie[1][0] == 'q' or (len(linie) == 1):
             for stare_finala in linie:
                 starifinale.append(stare_finala)
+                cititstare = True
     except:
         if len(linie) == 1:
-            cuvinte.append(linie[0])
+            if cititstare == False:
+                starifinale.append(linie[0])
+                cititstare = True
+            else:
+                cuvinte.append(linie[0])
+
+
+starifinale = ['q3']
+print(automat)
+print(starifinale)
 
 # ce bag in stari     [stare actuala, cuvant actual, traseu pana acum]
 for cuvant in cuvinte:
@@ -31,10 +45,10 @@ for cuvant in cuvinte:
     while len(Q) > 0:
         if len(Q[0][1]) > 0:
             if tuple([Q[0][0], Q[0][1][0]]) in automat:
-                new_element = [automat[tuple([Q[0][0], Q[0][1][0]])], Q[0][1][1:], Q[0][2]]
-                new_element[2].append(Q[0][0])
-
-                Q.append(new_element)
+                for el in automat[tuple([Q[0][0], Q[0][1][0]])]:
+                    new_element = [el, Q[0][1][1:], copy.deepcopy(Q[0][2])]
+                    new_element[2].append(Q[0][0])
+                    Q.append(new_element)
                 Q.pop(0)
             else:
                 Q.pop(0)
